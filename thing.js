2a68699f6a37624356c24e5b28014b1ce74a5c8a
Aaron Sikes <aaron@sikes.io>
2021-05-23 10:48:57 -0400

    Generate random messages

--- thing.js
+++ thing.js
@@ -1,3 +1,4 @@
+const alphabet='abcdefghijklmnopqrstuvwxyz'
 class Message {
   username;
   message;
@@ -43,7 +44,7 @@ class Chat {
 
 let right = document.querySelector('.right.chat-page');
 let left = document.querySelector('.left.chat-page');
-let page = new Chat(left, right);
+window.page = new Chat(left, right);
 page.append(new Message('aaron', 'heyyyOOO'));
 page.append(new Message('aaron', 'heyyyOOO'));
 page.append(new Message('aaron', 'heyyyOOO'));
@@ -51,7 +52,36 @@ page.append(new Message('aaron', 'heyyyOOO'));
 page.append(new Message('aaron', 'heyyyOOO'));
 page.append(new Message('aaron', 'heyyyOOO'));
 
-for (let i = 0; i<50; i++) {
-  page.append(new Message('aaron', 'heyyyOOO'));
+for (let i = 0; i<20; i++) {
+  page.append(new randoMessage());
 }
 
+function many(f, times, sep='') {
+  let result = '' + f();
+  for (let i=1; i<times; i++) {
+    result += sep + f();
+  }
+  return result;
+}
+
+function randoNumber(lower, upper) {
+  return lower + Math.floor(Math.random()*(upper-lower));
+}
+
+function randoSentence() {
+  let len = randoNumber(1, 20);
+  return many(randoWord, len, ' ') + '.';
+}
+
+function randoWord() {
+  let len = randoNumber(3, 10);
+  return many(randoLetter, len);
+}
+
+function randoLetter() {
+  return alphabet[Math.floor(Math.random()*26)];
+}
+
+function randoMessage() {
+  return new Message(randoWord(), randoSentence());
+}
