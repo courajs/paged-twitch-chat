31b7615da47bc3c5136c794d3c982abc3d2f0030
Aaron Sikes <aaron@sikes.io>
2021-05-23 16:18:01 -0400

    Initial PoC done! Actually switch back-and-forth between pages
    Use nice css animations for fade ins of messages and fade outs of pages

--- thing.js
+++ thing.js
@@ -11,48 +11,92 @@ class Message {
 
 const message_template = document.createElement('template');
 message_template.innerHTML = `
-    <p class="chat-message">
-      <span class="username"></span>
+    <p class="chat-message fade-in">
+      <span class="username"></span>:
       <span class="message-content"></span>
     </p>
 `;
 class Chat {
   left;
   right;
-  current = 'right';
+  current;
+  other;
   messages = [];
+  pending = [];
 
   constructor(left, right) {
-    this.left = left;
-    this.right = right;
+    this.left = this.current = left;
+    this.right = this.other = right;
+  }
+
+  on_message(msg) {
+    this.messages.push(msg);
+    if (this.pending.length) {
+      this.pending.push(msg);
+    } else {
+      if (!this.append(msg)) {
+        this.pending.push(msg);
+      }
+    }
   }
 
   append(msg) {
-    this.messages.push(msg);
-    let fragment = message_template.content.cloneNode(true);
-    let p = fragment.querySelector('.chat-message');
-    p.classList.add("chat-message");
-    p.querySelector('.username').textContent = msg.username;
-    p.querySelector('.message-content').textContent = msg.message;
-    if (this.current === 'right') {
-      this.right.append(p);
-      if (this.right.scrollHeight > this.right.clientHeight) {
-        this.current = 'left';
-        p.remove();
-        this.left.append(p);
-      }
-    } else if (this.current === 'left') {
-      this.left.append(p);
+    let p = render_message(msg);
+    this.current.append(p);
+    if (this.current.scrollHeight > this.current.clientHeight) {
+      p.remove();
+      [this.current, this.other] = [this.other, this.current];
+      clearPage(this.current).then(()=>this.printPending());
+      return false;
     }
+    return true;
   }
+
+  printPending() {
+    for (let i in this.pending) {
+      if (!this.append(this.pending[i])) {
+        this.pending.splice(0, i);
+      }
+    }
+    this.pending.length = 0;
+  }
+
+  appendRandom() {
+    this.on_message(randoMessage());
+  }
+}
+
+async function clearPage(page) {
+  if (!page.children.length) { return; }
+  let resolve;
+  let result = new Promise((r) => resolve = r);
+  page.classList.add('fade-children-out');
+  let handler = (event) => {
+    if (event.animationName === 'fadeOutOpacity') {
+      page.classList.remove('fade-children-out');
+      page.textContent = '';
+      page.removeEventListener('animationend', handler);
+      resolve();
+    }
+  };
+  page.addEventListener('animationend', handler);
+  return result;
+}
+
+function render_message(msg) {
+  let fragment = message_template.content.cloneNode(true);
+  let p = fragment.querySelector('.chat-message');
+  p.querySelector('.username').textContent = msg.username;
+  p.querySelector('.message-content').textContent = msg.message;
+  return p;
 }
 
 let right = document.querySelector('.right.chat-page');
 let left = document.querySelector('.left.chat-page');
 window.page = new Chat(left, right);
 
-for (let i = 0; i<20; i++) {
-  page.append(new randoMessage());
+for (let i = 0; i<27; i++) {
+  page.append(randoMessage());
 }
 
 function many(f, times, sep='') {
